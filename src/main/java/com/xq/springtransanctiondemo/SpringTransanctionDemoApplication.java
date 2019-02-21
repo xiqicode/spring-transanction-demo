24fbf540f9f09642fe97a73e0f5222072bc748d3
qi.xi <2440252489@qq.com>
2019-02-21 21:07:05 +0800

    init commit

---
+++ src/main/java/com/xq/springtransanctiondemo/SpringTransanctionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xq.springtransanctiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransanctionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransanctionDemoApplication.class, args);
+	}
+
+}
